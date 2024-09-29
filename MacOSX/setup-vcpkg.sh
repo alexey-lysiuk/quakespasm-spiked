a73703f79c77274e7a2974f389c3f229c0c73c3b
alexey.lysiuk <alexey.lysiuk@gmail.com>
2024-09-29 12:04:07 +0300

    no message

--- MacOSX/setup-vcpkg.sh
+++ MacOSX/setup-vcpkg.sh
@@ -2,7 +2,11 @@
 
 if [ ! -d "vcpkg" ]; then
     git clone --depth 1 https://github.com/microsoft/vcpkg
+    echo '[1] boostrap -- start'
     ./vcpkg/bootstrap-vcpkg.sh
+    echo '[1] boostrap -- end'
 fi
 
+echo '[1] install -- start'
 ./vcpkg/vcpkg install --overlay-triplets=custom-triplets --triplet=x64-osx-10.9 zlib libogg opus opusfile libvorbis
+echo '[1] install -- end'
